74c9f043ff5cbc078b7757519d993552d84a47fa
jrealpe <jreapupito@gmail.com>
2015-01-16 17:26:54 -0500

    BaseDAtos

--- PAJA_SYSTEM.sql
+++ PAJA_SYSTEM.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `PAJA_SYSTEM` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `PAJA_SYSTEM`;
 -- MySQL dump 10.13  Distrib 5.6.19, for linux-glibc2.5 (x86_64)
 --
 -- Host: localhost    Database: PAJA_SYSTEM
@@ -56,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-01-16 17:01:50
+-- Dump completed on 2015-01-16 17:26:30
