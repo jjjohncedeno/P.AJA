a87dea0262daf64ded6f958cf8453a72ac547bad
jrealpe <jreapupito@gmail.com>
2015-01-16 17:02:41 -0500

    BaseDAtos

--- PAJA_SYSTEM.sql
+++ PAJA_SYSTEM.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `PAJA_SYSTEM` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `PAJA_SYSTEM`;
 -- MySQL dump 10.13  Distrib 5.6.19, for linux-glibc2.5 (x86_64)
 --
 -- Host: localhost    Database: PAJA_SYSTEM
@@ -16,6 +14,38 @@ USE `PAJA_SYSTEM`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Personal`
+--
+
+DROP TABLE IF EXISTS `Personal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Personal` (
+  `personal_id` int(11) NOT NULL,
+  `personal_nombre` varchar(25) NOT NULL,
+  `personal_apellido` varchar(25) NOT NULL,
+  `personal_cedula` int(10) NOT NULL,
+  `personal_telefono` int(12) DEFAULT NULL,
+  `personal_tipo` varchar(25) NOT NULL,
+  `personal_direccion` varchar(40) NOT NULL,
+  `personal_sexo` varchar(12) NOT NULL,
+  `personal_correo` varchar(40) DEFAULT NULL,
+  `personal_carrera` varchar(30) DEFAULT NULL,
+  `personal_facultad` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`personal_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Personal`
+--
+
+LOCK TABLES `Personal` WRITE;
+/*!40000 ALTER TABLE `Personal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Personal` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -26,4 +56,4 @@ USE `PAJA_SYSTEM`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-01-16 15:09:06
+-- Dump completed on 2015-01-16 17:01:50
